8088ad992a851142ed8140e1aaf7602acbf15df9
Niklas Eicker <git@nikl.me>
2024-04-13 10:32:02 +0200

    Revert "load untyped"
    This reverts commit 3ea5d6c83548b845133145bb6b359695fad3d44d.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use bevy::asset::LoadedUntypedAsset;
 use bevy::prelude::*;
 use bevy_asset_loader::asset_collection::AssetCollection;
 use bevy_asset_loader::prelude::*;
@@ -10,42 +9,19 @@ pub(crate) struct TestAssets {
     pub torch: Handle<Image>,
 }
 
-#[derive(Resource)]
-struct Untyped(Handle<LoadedUntypedAsset>);
-
-fn load(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,) {
-    commands.spawn(Camera2dBundle::default());
-    commands.insert_resource(Untyped(asset_server.load_untyped("torch.png")));
-}
-
 fn spawn_camera(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    // test_assets: Res<TestAssets>,
+    test_assets: Res<TestAssets>,
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    // commands.spawn(SpriteBundle {
-    //     texture: asset_server.load_untyped("torch.png"),
-    //     // texture: test_assets.torch.clone(),
-    //     transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.)),
-    //     ..default()
-    // });
-}
-
-fn draw(mut commands: Commands,mut ran: Local<bool>, handle: Res<Untyped>, assets: Res<Assets<LoadedUntypedAsset>>) {
-    if *ran { return; }
-    println!("check");
-    if let Some(LoadedUntypedAsset{handle}) = assets.get(&handle.0) {
-        commands.spawn(SpriteBundle {
-            texture: handle.clone().typed(),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.)),
-            ..default()
-        });
-        *ran = true;
-    }
+    commands.spawn(SpriteBundle {
+        // texture: asset_server.load("torch.png"),
+        texture: test_assets.torch.clone(),
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.)),
+        ..default()
+    });
 }
 
 fn main() {
@@ -53,13 +29,12 @@ fn main() {
     app
         .add_plugins(DefaultPlugins)
 
-        // .init_collection::<TestAssets>()
+        .init_collection::<TestAssets>()
 
         .add_systems(
             Startup,
-            load,
+            spawn_camera,
         )
-        .add_systems(Update, draw)
 
         .run();
 }
