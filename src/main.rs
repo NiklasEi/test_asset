0d399d32b9d717363daf50896367cfe90aa1c93e
Niklas Eicker <git@nikl.me>
2024-04-13 10:59:14 +0200

    Reproduce issue without bevy_asset_loader

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
 use bevy::prelude::*;
-use bevy_asset_loader::asset_collection::AssetCollection;
-use bevy_asset_loader::prelude::*;
 
-
-#[derive(AssetCollection, Resource)]
+#[derive(Resource)]
 pub(crate) struct TestAssets {
-    #[asset(path = "torch.png")]
     pub torch: Handle<Image>,
+}
+
+impl FromWorld for TestAssets {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.resource::<AssetServer>();
+        TestAssets {
+            torch: { asset_server.load("torch.png") },
+        }
+    }
 }
 
 fn spawn_camera(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
     test_assets: Res<TestAssets>,
+    // asset_server: Res<AssetServer>
 ) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn(SpriteBundle {
-        // texture: asset_server.load("torch.png"),
         texture: test_assets.torch.clone(),
+        // texture: asset_server.load("torch.png"),
         transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.)),
         ..default()
     });
@@ -26,15 +31,8 @@ fn spawn_camera(
 
 fn main() {
     let mut app = App::new();
-    app
-        .add_plugins(DefaultPlugins)
-
-        .init_collection::<TestAssets>()
-
-        .add_systems(
-            Startup,
-            spawn_camera,
-        )
-
+    app.add_plugins(DefaultPlugins)
+        .init_resource::<TestAssets>()
+        .add_systems(Startup, spawn_camera)
         .run();
 }
